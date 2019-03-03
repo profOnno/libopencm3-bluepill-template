8e1e970fc2ade43f843fe9a9ab5ed52908c3bac7
Menno Vossen <nemno@kuopzi.nl>
2019-03-03 23:40:58 +0100

    after changes it now compiles

--- my-project/my-project.c
+++ my-project/my-project.c
@@ -1,9 +1,10 @@
-#include "api.h"
-#include "api-asm.h"
+#include "../my-common-code/api.h"
+#include <stdint.h>
+//#include "../my-common-code/api-asm.h"
 
 int main(void) {
 	/* add your own code */
 	uint32_t rev = 0xaabbccdd;
-	rev = rev_bytes(rev);
+//	rev = rev_bytes(rev);
 	return my_func(rev);
 }
